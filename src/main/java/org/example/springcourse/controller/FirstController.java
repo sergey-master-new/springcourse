7900769c165f77387ad9c00c0c5fe3888d93bb6c
sergey-master-new <prodvizh.com@gmail.com>
2023-02-08 21:47:08 +0300

    Add model and attribute thymeleaf

--- src/main/java/org/example/springcourse/controller/FirstController.java
+++ src/main/java/org/example/springcourse/controller/FirstController.java
@@ -1,35 +1,28 @@
 package org.example.springcourse.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;                                                 //Model - класс Spring-a
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
-@RequestMapping("/first")         //добавится в адрес url
+@RequestMapping("/first")
 public class FirstController {
 
-    @GetMapping("/hello")                                    //напр., http://localhost:8080/first/hello?name=Ivan&surname=Ivanov
-    public String helloPage(HttpServletRequest request) {    //c HttpServletRequest у нас полный доступ к запросу
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam (value = "name", required = false) String name,
+                            @RequestParam (value = "surname", required = false) String surname,
+                            Model model) {                                             //Spring автоматически внедрит Model model
 
-        System.out.println("Hello " + name + " " + surname);  //Hello Ivan Ivanov
-                                                              // если http://localhost:8080/first/hello ,то Hello null null
+        model.addAttribute("message", "Hello " + name + " " + surname);          // добавляем ключ - значение
 
-        return "first/hello";     //html-страница будет находиться в папке first, hello -> hello.html
+
+        return "first/hello";                                     // Model model будет передана в представление view
     }
 
-    @GetMapping("/goodbue")                                     //напр.,http://localhost:8080/first/goodbue?name=Ivan&surname=Ivanov
-    public String goodBuePage(@RequestParam String name,
-                              @RequestParam (value = "surname", required = false) String surname) {   //surname - можно не указывать
-
-        System.out.println("Goodbue " + name + " " + surname);  // Goodbue Ivan Ivanov
-                                                                // ошибка 400 – Bad Request, если http://localhost:8080/first/goodbue
-                                                                // Goodbue Ivan null, если http://localhost:8080/first/goodbue?name=Ivan
-
-        return "first/goodbue";  //html-страница будет находиться в папке first, goodbue -> goodbue.html
+    @GetMapping("/goodbue")
+    public String goodBuePage() {
+        return "first/goodbue";                                    // html-страница будет находиться в папке first, goodbue -> goodbue.html
     }
 }
